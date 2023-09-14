d922da6a8bbcf8b9e392dc39f9bfcb045323db8f
Vritant-Goel <114694266+Vritant-Goel@users.noreply.github.com>
2023-09-15 04:13:16 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,53 +2,49 @@ import numpy as np
 import cv2
 
 	
-# Read the image
+# Reading image
 image = cv2.imread('digits.png')
 
 # gray scale conversion
 gray_img = cv2.cvtColor(image,
 						cv2.COLOR_BGR2GRAY)
 
-# We will divide the image
-# into 5000 small dimensions
-# of size 20x20
+# divide image -> 5000 small dimensions
+# size 20x20
 divisions = list(np.hsplit(i,100) for i in np.vsplit(gray_img,50))
 
 # Convert into Numpy array
-# of size (50,100,20,20)
+# size -> (50,100,20,20)
 NP_array = np.array(divisions)
 
-# Preparing train_data
-# and test_data.
-# Size will be (2500,20x20)
+# Preparing train_data and test_data.
+# Size -> (2500,20x20)
 train_data = NP_array[:,:50].reshape(-1,400).astype(np.float32)
 
-# Size will be (2500,20x20)
+# Size -> (2500,20x20)
 test_data = NP_array[:,50:100].reshape(-1,400).astype(np.float32)
 
-# Create 10 different labels
-# for each type of digit
+# Creating 10 different labels for each type of digit
 k = np.arange(10)
 train_labels = np.repeat(k,250)[:,np.newaxis]
 test_labels = np.repeat(k,250)[:,np.newaxis]
 
-# Initiate kNN classifier
+# Initiating kNN classifier
 knn = cv2.ml.KNearest_create()
 
-# perform training of data
+# training of data
 knn.train(train_data,
 		cv2.ml.ROW_SAMPLE,
 		train_labels)
 
-# obtain the output from the
-# classifier by specifying the
+# obtain output from classifier by specifying the
 # number of neighbors.
 ret, output ,neighbours,
 distance = knn.findNearest(test_data, k = 3)
 
-# Check the performance and
+# Checking performance and
 # accuracy of the classifier.
-# Compare the output with test_labels
+# Compare test_labels and output
 # to find out how many are wrong.
 matched = output==test_labels
 correct_OP = np.count_nonzero(matched)
